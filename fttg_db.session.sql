41ded8a6b7845df6a3e65eeb0d0bb24ffbc9a479
Tech Guy <femithetechguy@gmail.com>
2025-05-03 00:29:20 -0400

    selecttion  and filtering queries completed

---
+++ fttg_db.session.sql
@@ -0,0 +1,52 @@
+-- SELECT table_name 
+-- FROM information_schema.tables
+-- WHERE table.schema = 'public';
+
+-- SELECTION AND FILTERING
+-- Imagine a student table
+
+-- Create student table
+CREATE TABLE student (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    grade VARCHAR(10)
+);
+
+-- Insert Value into student table
+INSERT INTO student (name, age, grade) VALUES
+('Alice', 20, 88),
+('Bob', 22, 92),
+('Charlie', 21, 85),
+('David', 23, 90),
+('Eve', 20, 95);
+
+-- Confirm student table was created and exists
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema ='public'
+ORDER BY table_name;
+
+-- View the data in the student table, order by id
+SELECT * FROM student ORDER BY id;
+)
+-- TASK :   Show only the name and grade,
+-- and age of students
+--  who are less than 20
+-- select columns name and grade
+-- filterby age < 20
+
+SELECT name, grade, age
+FROM student
+WHERE age < 20;
+
+Outcome: We display a subset of our student with only the name and grade columns.
+
+-- Change one student age to less than 20 - Bob
+UPDATE student
+SET age = 19
+WHERE name ='Bob';
+
+JOIN - INNER. LEFT,RIGHT
+DELETE FROM student
+WHERE id = 5;
